96c11051cedd8be9d11efb3c09464f29d6085e1b
AbbyssalSKD <abbyssalskd@gmail.com>
2026-05-12 12:35:38 -0700

    完善了加速度系统，新增并完成了郊狼时间计时器，从现在开始，玩家在离开平台边缘的一小段时间内也可以进行第一次起跳

--- scene/player.gd
+++ scene/player.gd
@@ -13,23 +13,28 @@ enum FACING {
 }
 
 var gravity := ProjectSettings.get("physics/2d/default_gravity") as float 
-var run_speed : float = 200.0
-var jump_velocity : float = -400.0
-
+const run_speed : float = 200.0
+const jump_velocity : float = -300.0
+const floor_acc : float  = run_speed/0.2
+const air_acc : float  = run_speed/0.4
 var action_state : STATE = STATE.floor
 var facing_state : FACING = FACING.R
 var can_double_jump : bool = false
 var hold_jump : bool = false
+var has_jumped := false
 @export var jump_impede = -5
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var coyote_timer: Timer = $CoyoteTimer
 
 
 func _physics_process(delta: float) -> void:
 	
+	var was_on_flool := is_on_floor()
 	var dir := Input.get_axis("left", "right")
-	velocity.x =dir * run_speed
+	var acc := floor_acc if is_on_floor() else air_acc	
+	velocity.x =move_toward(velocity.x, dir * run_speed, delta * acc)
 	velocity.y += gravity * delta
 	
 	match action_state:
@@ -42,12 +47,8 @@ func _physics_process(delta: float) -> void:
 				animation_player.play("idle")
 			
 			
-			if Input.is_action_just_pressed("jump"):
-				animation_player.play("jump_up")
-				velocity.y = jump_velocity
-				hold_jump = true
-				can_double_jump = true
-				action_state = STATE.jump
+			if Input.is_action_just_pressed("jump") and not has_jumped and can_1st_jump():
+				do_jump()
 			
 			if not is_on_floor():
 				can_double_jump = true
@@ -73,18 +74,24 @@ func _physics_process(delta: float) -> void:
 			
 			if not is_on_floor():
 				velocity.y += gravity * delta
-
-			if is_on_floor(): 
+			else: 
 				action_state = STATE.floor
 				can_double_jump = false
+				has_jumped = false
 			
-			if Input.is_action_just_pressed("jump") and can_double_jump:
+			if Input.is_action_just_pressed("jump") and has_jumped and can_double_jump:
 				do_double_jump()
+			elif Input.is_action_just_pressed("jump") and not has_jumped and coyote_timer.is_stopped():
+				do_jump()	
 				
 			if hold_jump and velocity.y <= jump_impede:
 				velocity.y += jump_impede
-	
-		
+				
+			
+			
+			if Input.is_action_just_released("jump"):
+				hold_jump = false
+				
 	match facing_state:
 		FACING.R:
 			sprite_2d.flip_h = false
@@ -100,9 +107,28 @@ func _physics_process(delta: float) -> void:
 	print (can_double_jump, action_state,' ', velocity.y)
 	
 	move_and_slide()
+	
+	if is_on_floor() != was_on_flool: 
+		if was_on_flool and action_state == STATE.fall:
+			coyote_timer.start()
+		else:
+			coyote_timer.stop()
+		
 
 func do_double_jump() -> void:
 	animation_player.play("jump_up")
 	action_state = STATE.jump
 	velocity.y = jump_velocity
 	can_double_jump = false
+
+func do_jump() -> void:
+	animation_player.play("jump_up")
+	velocity.y = jump_velocity
+	hold_jump = true
+	can_double_jump = true
+	action_state = STATE.jump
+	coyote_timer.stop()
+	has_jumped = true
+	
+func can_1st_jump() -> bool:
+	return is_on_floor() or  coyote_timer.time_left > 0
